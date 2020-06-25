86aa8c4f2d8f7961d554e0b10f9dbf1bfd36a052
florian peyron <florian.peyron@gogowego.com>
2020-06-25 14:05:35 +0200

    Basic prisme response reading

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express = require("express");
-// Create a new express app instance
+import bodyParser from "body-parser";
 
 const app: express.Application = express();
+app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  console.log('got request', req);
-  res.send("Hello World!");
+
+app.post("/", function (request) {
+  const { body: { intent: { inputs = {}, name = "" } = {}, fulfillment = {}, query = '' } = {} } = request;
+  console.log('got request', name, fulfillment, query);
+  return 0;
 });
 app.listen(3000, function () {
   console.log("App is listening on port 3000!");
